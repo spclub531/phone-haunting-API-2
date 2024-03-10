9762d67c42a4f54a6f72c01df5fa43dc4fcb558a
suraj <surajlalapatel@gmail.com>
2024-03-10 12:54:51 +0530

    changes done

--- script.js
+++
@@ -1,9 +0,0 @@
-function updateTime() {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    document.getElementById('time').innerText = `${hours}:${minutes}:${seconds}`;
-}
-
-setInterval(updateTime, 1000);
